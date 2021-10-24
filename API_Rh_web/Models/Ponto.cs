78d98082f8d438420720ff1db858a036a36be704
willian156 <willian1561@outlook.com>
2021-10-24 01:35:20 -0300

    Commit para salvar

--- API_Rh_web/Models/Ponto.cs
+++ API_Rh_web/Models/Ponto.cs
@@ -8,8 +8,10 @@ namespace API_Rh_web.Models
         [Required]
         [Key]
         public int id_vaga { get; set; }
+        public Vaga vaga { get; set; }
         [Required]
         public int id_Conhecimentos { get; set; }
+        public Conhecimento conhecimento { get; set; }
         public int pontos { get; set; }
     }
 }
